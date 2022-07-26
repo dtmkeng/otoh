3dbc0c87f8131709ca49d0ec9475533946358612
dtmkeng <phumaisut@gmail.com>
2022-07-26 22:21:15 +0700

    set scoll bar and save in local storage

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+import { onMount } from "svelte";
+import dayjs from 'dayjs';
+  const keyListSentence  = "list-sentect-key";
   let word ="";
   let sentence ="";
   $: listSentence = [{word: "keng", sentence: "keng is the new people in the world he is a developer at ODDS", date: new Date(2022, 7, 10, 11, 11, 10)}];
@@ -8,34 +11,43 @@
     wordCount = sentence.split(' ').filter(e => e.length > 0).length;
   }
 
+  onMount(async () => {
+		const list = window.localStorage.getItem(keyListSentence);
+    if (list) {
+      listSentence = JSON.parse(list);
+    }
+	});
+
   const onSave = () => {
-    listSentence = [{
-      word,
-      sentence,
-      date: new Date()
-    }, ...listSentence];
-    word = "";
-    sentence = "";
-    console.log(listSentence)
+    if (word.length && sentence.length) {
+      listSentence = [{
+        word,
+        sentence,
+        date: new Date()
+      }, ...listSentence];
+      word = "";
+      sentence = "";
+      localStorage.setItem(keyListSentence, JSON.stringify(listSentence));
+    }
   }
 </script>
 
 <main>
-  <div>
+  <div class="container">
     <p>What is the word for today</p>
     <input type="text" bind:value={word} class="one-word-input" placeholder="What is the word for today">
     <div class="word-to-senten">
       <textarea  id="sentence" cols="100" rows="10" bind:value={sentence} on:keydown={() => onSentenceChange()}></textarea>
       <span class="number-of-word">Number of word: {wordCount}</span>
       <div class="save-container">
-        <button class="px" on:click={() => onSave()}>Save</button>
+        <button class="px" on:click={() => onSave()} disabled={wordCount < 20 || word.length === 0}>Save</button>
       </div>
     </div>
-    <div>
+    <div class="sentence-list">
       {#each listSentence as item}
-        <div>
+        <div class="list">
           <div class="title">{item.word}</div>
-          <span class="when">{item.date}</span>
+          <span class="when">{dayjs(item.date).locale('th').toString()}</span>
           <p>{item.sentence}</p>
         </div>
       {/each}
@@ -44,6 +56,20 @@
 </main>
 
 <style>
+  .sentence-list {
+    margin-top: 32px;
+    overflow: auto;
+    height: 560px;
+  }
+
+  .list {
+    margin-bottom: 14px;
+  }
+
+  .container {
+    flex-direction: column;
+    display: flex;
+  }
   .when {
     font-size: 12px;
   }
